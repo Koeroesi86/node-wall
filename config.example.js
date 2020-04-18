28b4d9ac1424145969329a18346eee368ad3fa8c
Krisztian Korosi <koeroesi86@gmail.com>
2020-04-18 10:46:55 +0100

    Upgraded @koeroesi86/node-webserver

--- config.example.js
+++ config.example.js
@@ -14,19 +14,17 @@ module.exports = {
   },
   workerOptions: {
     root: 'D:\\Chris\\Documents\\Developement\\uzenofal\\public',
-    options: {
-      env: {
-        DB_DRIVER: process.env.DB_DRIVER || 'sqlite3',
-        DB_DATABASE: process.env.DB_DATABASE || 'D:/Chris/Documents/Developement/uzenofal/lib/data/wall.sqlite',
-        NODEMAILER_HOST: process.env.NODEMAILER_HOST,
-        NODEMAILER_PORT: process.env.NODEMAILER_PORT,
-        NODEMAILER_USER: process.env.NODEMAILER_USER,
-        NODEMAILER_PASS: process.env.NODEMAILER_PASS,
-        SITE_PROTOCOL: process.env.SITE_PROTOCOL,
-        SITE_DOMAIN: process.env.SITE_DOMAIN,
-      },
-      cwd: 'D:\\Chris\\Documents\\Developement\\uzenofal',
+    env: {
+      DB_DRIVER: process.env.DB_DRIVER || 'sqlite3',
+      DB_DATABASE: process.env.DB_DATABASE || 'D:/Chris/Documents/Developement/uzenofal/lib/data/wall.sqlite',
+      NODEMAILER_HOST: process.env.NODEMAILER_HOST,
+      NODEMAILER_PORT: process.env.NODEMAILER_PORT,
+      NODEMAILER_USER: process.env.NODEMAILER_USER,
+      NODEMAILER_PASS: process.env.NODEMAILER_PASS,
+      SITE_PROTOCOL: process.env.SITE_PROTOCOL,
+      SITE_DOMAIN: process.env.SITE_DOMAIN,
     },
+    cwd: 'D:\\Chris\\Documents\\Developement\\uzenofal',
     limit: 0,
     limitPerPath: 1,
     index: [
